651b0e75ac713265b3a1434e29d1fc0b6eea70ea
dragonicivictor-stack <144392976+dragonicivictor-stack@users.noreply.github.com>
2023-10-15 17:51:16 +0300

    incarcare solutie min si max recursiv

--- OopTheoryFirstPart/Program.cs
+++ OopTheoryFirstPart/Program.cs
@@ -23,6 +23,17 @@ namespace OopTheoryFirstPart
 
             int[] array2 = new[] { 1, 2, 3, 4 };
             RecursivityHelper.Iterate(array2);
+
+
+            // minim si maxim recursiv
+
+            int[] arrayTestVictor = { 3, 7, 2, 8, 6, 4, 9 };
+
+            Console.WriteLine("Max Value: " + ArrayHelper.MaxRecursiv(arrayTestVictor, arrayTestVictor.Length - 1));
+            Console.WriteLine("Min Value: " + ArrayHelper.MinRecursiv(arrayTestVictor, arrayTestVictor.Length - 1));
+
+            //doar ca sa nu se inchida terminalul
+            Console.ReadLine();
         }
     }
 }
---
+++ OopTheoryLibrary/ArrayHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace OopTheoryLibrary
+{
+	public class ArrayHelper
+	{
+		
+
+        public static int MinRecursiv(int[] arr, int index)
+        {
+
+            //verificam daca vectorul este null sau empty si ridicam exceptie
+            if (arr == null || arr.Length == 0)
+                throw new ArgumentException("Exceptie null sau empty");
+
+            // daca indexul e 0 returnam gol
+            if (index == 0)
+                return arr[0];
+
+            int currentMin = MinRecursiv(arr, index - 1);
+            if (arr[index] < currentMin)
+                return arr[index];
+            else
+                return currentMin;
+        }
+
+        public static int MaxRecursiv(int[] arr, int index)
+        {
+
+            //verificam daca vectorul este null sau empty si ridicam exceptie
+            if (arr == null || arr.Length == 0)
+                throw new ArgumentException("Exceptie null sau empty");
+
+            // daca indexul e 0 returnam gol
+            if (index == 0)
+                return arr[0];
+
+            int currentMax = MaxRecursiv(arr, index - 1);
+            if (arr[index] > currentMax)
+                return arr[index];
+            else
+                return currentMax;
+        }
+    }
+    
+}
+
